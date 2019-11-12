2149738412c81e000ecda50d297806678322b70c
marley <marleyreis234@gmail.com>
2019-11-12 21:09:55 -0200

    Fim Metodos Usuario

--- ProjetoCRUDADS/src/univs/edu/usuario/UsuarioDAO.java
+++ ProjetoCRUDADS/src/univs/edu/usuario/UsuarioDAO.java
@@ -1,23 +1,56 @@
 package univs.edu.usuario;
 
+import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import static org.hibernate.criterion.Projections.id;
+import org.hibernate.criterion.Restrictions;
 import univs.edu.util.HibernateUtil;
 
-
 public class UsuarioDAO {
-    
+
     private Session sessao;
     private Transaction transacao;
-    
-    public void salvar(Usuario usuario){
+
+    public void salvar(Usuario usuario) {
         sessao = HibernateUtil.
                 getSessionFactory().openSession();
         transacao = sessao.beginTransaction();
-        sessao.save(usuario);
+        if (usuario.getIdUsuario() == 0) {
+
+            sessao.save(usuario);
+        } else {
+
+            sessao.update(usuario);
+
+            transacao.commit();
+            sessao.close();
+        }
+    }
+
+    public void excluir(Usuario usuario) {
+        sessao = HibernateUtil.
+                getSessionFactory().openSession();
+        transacao = sessao.beginTransaction();
+        sessao.delete(usuario);
         transacao.commit();
         sessao.close();
     }
-    
-    
+    public Usuario pesquisar(int id){
+          sessao = HibernateUtil.
+                getSessionFactory().openSession();
+        transacao = sessao.beginTransaction();
+        Usuario usuario = (Usuario) sessao.createCriteria(Usuario.class).add(Restrictions.eq("idUsuario", id)).uniqueResult();
+        return usuario;
+        
+    }
+     public List<Usuario> pesquisar(){
+          sessao = HibernateUtil.
+                getSessionFactory().openSession();
+        transacao = sessao.beginTransaction();
+        List<Usuario> usuarios = sessao.createCriteria(Usuario.class).list();
+         return usuarios;
 }
+     
+}
+
